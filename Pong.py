9ea9209ea402399571a464520e169819701fa9cb
Caleb Brown <cbrown200@west-mec.org>
2020-09-13 15:39:11 -0700

    Pong

---
+++ Pong.py
@@ -0,0 +1,21 @@
+#Pong
+
+import turtle
+
+#window 
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Paddle A
+
+#Paddle B
+
+#Ball
+
+
+#Main Game loop
+while True:
+    wn.update()
